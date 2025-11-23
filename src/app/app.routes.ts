d910afc1666dc3c83f3dfeed1aefbf60bcb82ed5
Raet0 <Yosoyrafaelprieto@gmail.com>
2025-11-22 23:52:01 -0500

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { EstilosPage } from './features/estilos-page/estilos-page';
-
-export const routes: Routes = [
-  {
-  path:"",
-  component: EstilosPage
-  },  
-];
